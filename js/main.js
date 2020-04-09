d3dfbf8570cd2bf7c0c5e6a8b3354d61673ecfa2
Flavio Pinto <flaviopnt@hotmail.it>
2020-04-09 19:13:55 +0200

    Left and Right button script - Completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 $(document).ready(function () {
-    
+    // Variabili
+    var buttonLeft = $('.button-left');
+    var buttonRight = $('.button-right');
+    var imgContainer = $('.img-container');
+
+    buttonRight.click(nextImg);
+    buttonLeft.click(previousImg);
 }); // fine document ready
+
+
+
+/* Funzioni */
+
+// Funzione per pulsante immagine successiva
+function nextImg() {
+    var activeImg = $('.img-container.show');
+    activeImg.removeClass('show');
+    if (activeImg.hasClass('last-img')) {
+        $('.img-container.first-img').addClass('show');
+    } else {
+        activeImg.next().addClass('show');
+    }
+}
+
+// Funzione per pulsante immagine successiva
+function previousImg() {
+    var activeImg = $('.img-container.show');
+    activeImg.removeClass('show');
+    if (activeImg.hasClass('first-img')) {
+        $('.img-container.last-img').addClass('show')
+    } else {
+        activeImg.prev().addClass('show');
+    }
+}
