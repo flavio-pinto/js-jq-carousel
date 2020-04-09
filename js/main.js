0cf1f1adeff9bc2b00455aca3da1db1905d0b29b
Flavio Pinto <flaviopnt@hotmail.it>
2020-04-09 19:31:27 +0200

    Keyboard input script - Completed

--- js/main.js
+++ js/main.js
@@ -3,33 +3,45 @@ $(document).ready(function () {
     var buttonLeft = $('.button-left');
     var buttonRight = $('.button-right');
     var imgContainer = $('.img-container');
+    
 
     buttonRight.click(nextImg);
     buttonLeft.click(previousImg);
+
+    $(document).keydown(function(event) {
+        if (event.keyCode == 39) {
+            nextImg()
+        } else if (event.keyCode == 37) {
+            previousImg();
+        }
+    });
+
+    
+    /* Funzioni */
+
+    // Funzione per pulsante immagine successiva
+    function nextImg() {
+        var activeImg = $('.img-container.show');
+        activeImg.removeClass('show');
+        if (activeImg.hasClass('last-img')) {
+            $('.img-container.first-img').addClass('show');
+        } else {
+            activeImg.next().addClass('show');
+        }
+    }
+
+    // Funzione per pulsante immagine successiva
+    function previousImg() {
+        var activeImg = $('.img-container.show');
+        activeImg.removeClass('show');
+        if (activeImg.hasClass('first-img')) {
+            $('.img-container.last-img').addClass('show')
+        } else {
+            activeImg.prev().addClass('show');
+        }
+    }
 }); // fine document ready
 
 
 
-/* Funzioni */
 
-// Funzione per pulsante immagine successiva
-function nextImg() {
-    var activeImg = $('.img-container.show');
-    activeImg.removeClass('show');
-    if (activeImg.hasClass('last-img')) {
-        $('.img-container.first-img').addClass('show');
-    } else {
-        activeImg.next().addClass('show');
-    }
-}
-
-// Funzione per pulsante immagine successiva
-function previousImg() {
-    var activeImg = $('.img-container.show');
-    activeImg.removeClass('show');
-    if (activeImg.hasClass('first-img')) {
-        $('.img-container.last-img').addClass('show')
-    } else {
-        activeImg.prev().addClass('show');
-    }
-}
